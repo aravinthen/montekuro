8deb4ba88e4e5ea9a7a447042b9186f0634402bc
Aravinthen <arre0raj@gmail.com>
2022-07-06 19:29:12 +0100

    "getting back into using git. lot of commits!"

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # MonteKuro
 Experiments with Monte Carlo simulations for fun and profit, following *Understanding Molecular Simulations* by Frenkel and Smit.
+
 Kuro is my cat.
 
 ## Program structure
+The code is intended to be as modular as possible.
 
+## Running instructions
+Running is relatively self-contained. Have a look at the examples file for the simulation that you wish to run: if you have a simulation in mind, make your own example. Keep the name of your example file in mind!
+
+Then, edit `makefile` by replacing the `FILE` name with the name of the example file you wish to compile.
+
+Compile using the `make` command and your Monte Carlo simulation will await you as an executable program with the same name as the example file.
--- examples/metropolis.c
+++ examples/metropolis.c
@@ -6,29 +6,80 @@
 #include <math.h>
 #include <time.h>
 
-#include "../lib/system.h"
+#include "../lib/lib.h"
 
 int main(void){
-	int num_parts = 100; // the number of particles
-	int num_types = 3; // the number of types 
-	
-	// the dimensions of the problem
-	double xdim = 1.0;
-	double ydim = 1.0;
-	double zdim = 1.0;
+  srand(time(0)); // generate seed (this has to be done once per program)
+  
+  int num_parts = 5; // the number of particles per type
+  int num_types = 2; // the number of types
 
-	// initialize the type matrix.
-	struct type_matrix type_data = init_types(num_types);
-	define_sigma(&type_data, 1, 1, 1.0);
+  // here, we're modelling a binary fluid
+  int total = 2*num_parts; // the number of particles in the system
 
-	// the system: all particle information is contained here. 
-	struct particle system[num_parts];	
+  int num_moves = 100;
 
-	// add particles to the system
-	for (int i=0; i++; i<num_parts){
-		add_particle(system, i, 1, xdim, ydim, zdim);
-	}
+  // the dimensions of the problem
+  double xdim = 1.0;
+  double ydim = 1.0;
+  double zdim = 1.0;
 
-	// free the type matrices.
-	free_types(type_data);
+  // energy parameters
+  double cutoff=1.5;
+
+  // Global parameters
+  double energy, new_en;
+  
+  // initialize the type matrix.
+
+  struct type_matrix type_data = init_types(num_types);  
+  set_sigma(type_data.sigma, 1,1, 1.0);
+  set_sigma(type_data.sigma, 0,0, 1.0);
+  set_sigma(type_data.sigma, 1,0, 1.0);
+
+  set_epsilon(type_data.epsilon, 1,1, 1.0);
+  set_epsilon(type_data.epsilon, 0,0, 0.5);
+  set_epsilon(type_data.epsilon, 1,0, 0.2);
+
+  // the system: all particle information is contained here.
+  struct particle sys[total];
+  // the test state that is used in energy evaluations.
+  struct particle test[total]; 
+
+  // add particles to the system
+  for (int i=0; i<num_parts; i++){
+    add_particle(sys, i, 0, xdim, ydim, zdim);
+  }
+  for (int j=num_parts; j<total; j++){
+    add_particle(sys, j, 1, xdim, ydim, zdim);
+  }
+  
+  // assign the system particles to the test system
+  for (int k=0; k<num_moves; k++){
+    // assign the system in use to the test system
+    copy_system(test, sys, total);
+    
+    // move only the test system!
+    new_en = particle_move(test,
+			   type_data,
+			   2,
+			   1.0,
+			   total,
+			   cutoff,
+			   xdim, ydim, zdim);
+    
+    // calcuate the energy of the test system
+    energy = system_energy(test,
+			   type_data,
+			   total,
+			   cutoff,
+			   xdim, ydim, zdim);
+
+    printf("%f %f\n", energy, new_en);
+  };
+  
+  printf("Success!\n");
+  
+  // free the type matrices.
+  free_types(type_data);
 }
